90e4f7e19a3b5c6fdcc2554d46cbd1282a9b192a
epicgunshine <aphung@chicagotechacademy.org>
2015-10-03 17:47:22 +0000

    organized bell ringer files

---
+++ bell_ringer/5.2.rb
@@ -0,0 +1,12 @@
+puts "What is your 1st wish?"
+first_wish = gets.chomp
+
+puts "What is your 2nd wish?"
+second_wish = gets.chomp
+
+
+puts "What is your 3rd wish?"
+third_wish = gets.chomp
+
+
+puts "Your wishes were #{first_wish}, #{second_wish}, and #{third_wish}."
